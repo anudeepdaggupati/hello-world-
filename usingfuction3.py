23b42d97b97620ca8120bfe899ba8c8c706a50a5
anudeepdaggupati <42990293+anudeepdaggupati@users.noreply.github.com>
2018-09-26 14:48:28 +0530

    Add files via upload

---
+++ usingfuction3.py
@@ -0,0 +1,24 @@
+#write a program usingfuction
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+
+
+def add():
+	return a+b
+
+def sub():
+	return a-b
+
+def mul():
+	return a*b
+
+def div():
+	return a/b
+
+
+print("addition=",add())
+print("subtraction=",sub())
+print("multiplication=",mul())
+print("divison=",div())
+
+
