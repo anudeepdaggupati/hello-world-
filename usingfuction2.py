45c3bb54c5dde89421384b8f63ccb7affc6ab7b0
anudeepdaggupati <42990293+anudeepdaggupati@users.noreply.github.com>
2018-09-26 14:11:20 +0530

    Add files via upload

---
+++ usingfuction2.py
@@ -0,0 +1,16 @@
+#write a program using fuction
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+
+def add():
+	return a+b
+
+def sub():
+	return a-b
+
+print(add())
+print(sub())
+
+
+
+ 	
