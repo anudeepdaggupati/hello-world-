df90287b9104ec2eaf216b192a599c64fc594fa2
anudeepdaggupati <42990293+anudeepdaggupati@users.noreply.github.com>
2018-09-26 10:24:13 +0530

    Add files via upload

---
+++ using function.py
@@ -0,0 +1,14 @@
+#write ap program using fuction
+a=(int(input("enter the value of a"))
+b=(int(input("enter the value of b"))
+
+def add():
+	c=a+b
+	return c
+def sub():
+	return a-b
+
+
+	print(add())
+	print(sum())
+	
---
+++ usingfuction.py
@@ -0,0 +1,16 @@
+#write ap program using fuction
+a=(int(input("enter the value of a"))
+b=(int(input("enter the value of b"))
+
+
+
+def add():
+	return a+b
+
+
+def sub():
+	return a-b
+
+
+print(add())
+print(sub())
