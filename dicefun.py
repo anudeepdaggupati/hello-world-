2976ee1130f15498885a4b4f6614901706a13afd
anudeepdaggupati <42990293+anudeepdaggupati@users.noreply.github.com>
2018-09-28 09:42:59 +0530

    Add files via upload

---
+++ dicefun.py
@@ -0,0 +1,53 @@
+# 
+import random 
+count=0
+def myroll():
+	return random.randint(1,6)
+
+while(count<=100):
+	n=input("press r to roll the dice")
+	if(n=='r'):
+		r=myroll()
+		count=count+r
+		print("u got",r)
+		print("new position is",count)
+
+
+		if(count==8):
+			count=37
+			print("i got the ladder")
+		elif(count==11):
+			count=2
+			print("snake has bitten")
+		elif(count==13):
+			count=34
+			print("i dot the ladder")
+		elif(count==25):
+			count=4
+			print("snake has bitten")
+		elif(count==38):
+			count=9
+			print("snake has bitten")
+		elif(count==40):
+			count=68
+			print("i got the ladder")
+		elif(count==52):
+			count=81
+			print("i got the ladder")
+		elif(count==65):
+			count=46
+			print("snake has bitten")
+		elif(count==89):
+			count=70
+			print("snake has bitten")
+		elif(count==76):
+			count=97
+			print("i got the ladder")
+		elif(count==93):
+			count=64
+			print("snake has bitten")
+		elif(count>100):
+			count=count-r
+			print(count)
+		elif(count==100):
+			print("won the game")
